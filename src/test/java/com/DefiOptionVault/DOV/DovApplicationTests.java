34c716bc33e9a54a51441cd8ae7a22c0a4c6bd58
junan-98 <junwonseo00@gmail.com>
2023-11-10 14:29:41 +0900

    어렵게 구한 wrapper

--- src/test/java/com/DefiOptionVault/DOV/DovApplicationTests.java
+++ src/test/java/com/DefiOptionVault/DOV/DovApplicationTests.java
@@ -2,12 +2,115 @@ package com.DefiOptionVault.DOV;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.FunctionReturnDecoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.*;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCall;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootTest
 class DovApplicationTests {
+	private final String USDC = "0xe059aA96255990826D0d62c62462Feea47AF82a7";
+	private final String DOV_ADDRESS = "0xC1f89aE003F0264792C3Cf985c3bCAA8c1C7b3C0";
+	private final String ADMIN_ADDRESS = "0x6472A2F7187C49f05022b4F04b8f4545a7f88797";
+	private final String PRIVATE_KEY = "0x4e09642cccd2e9bbabb4f1136aa128986d46037e953cc21006f489a547ff995f";
+	private final String RPC = "https://polygon-mumbai.g.alchemy.com/v2/-cnnQMgoJVRgbeWlXVmUWJnn4YylXDng";
 
 	@Test
-	void contextLoads() {
+	void balanceOf() throws IOException {
+		// Web3j 초기화
+		Web3j web3j = Web3j.build(new HttpService(RPC));
+
+		// 호출할 함수와 파라미터 설정
+		String functionName = "balanceOf";
+		String accountAddress = "0x6472A2F7187C49f05022b4F04b8f4545a7f88797"; // 조회할 계정 주소
+		List<Type> inputParameters = Collections.singletonList(new Address(accountAddress));
+		List<TypeReference<?>> outputParameters = Collections.singletonList(new TypeReference<org.web3j.abi.datatypes.Uint>() {});
+
+		// 함수 객체 생성
+		Function function = new Function(
+				functionName,
+				inputParameters,
+				outputParameters);
+
+		// 트랜잭션을 인코딩
+		String encodedFunction = FunctionEncoder.encode(function);
+
+		// 함수 호출
+		EthCall response = web3j.ethCall(
+						org.web3j.protocol.core.methods.request.Transaction.createEthCallTransaction(
+								null, USDC, encodedFunction),
+						DefaultBlockParameterName.LATEST)
+				.send();
+
+		// 결과를 디코딩
+		List<Type> result = FunctionReturnDecoder.decode(
+				response.getValue(), function.getOutputParameters());
+
+		// 결과 출력
+		if (!result.isEmpty()) {
+			BigInteger balance = (BigInteger) result.get(0).getValue();
+			System.out.println("Balance of account " + accountAddress + " is: " + balance);
+		} else {
+			System.out.println("Empty response");
+		}
 	}
 
+	@Test
+	void bootstrap() throws IOException {
+		// Web3j 초기화
+		Web3j web3j = Web3j.build(new HttpService(RPC));
+
+		// 자격 증명 및 트랜잭션 매니저 설정
+		Credentials credentials = Credentials.create(PRIVATE_KEY);
+		TransactionManager transactionManager = new RawTransactionManager(web3j, credentials);
+		ContractGasProvider gasProvider = new DefaultGasProvider();
+
+		// 호출할 함수와 파라미터 설정
+		String functionName = "bootstrap";
+		List<Uint256> strikes = Arrays.asList(new Uint256(BigInteger.valueOf(1000)), new Uint256(BigInteger.valueOf(2000)));
+		Uint256 expiry = new Uint256(BigInteger.valueOf(1234567890));
+		Utf8String expirySymbol = new Utf8String("EXPIRY_SYMBOL");
+		List<Type> inputParameters = Arrays.asList(new DynamicArray<>(Uint256.class, strikes), expiry, expirySymbol);
+		List<TypeReference<?>> outputParameters = Arrays.asList();
+
+		// 함수 객체 생성
+		Function function = new Function(
+				functionName,
+				inputParameters,
+				outputParameters);
+
+		// 트랜잭션을 인코딩
+		String encodedFunction = FunctionEncoder.encode(function);
+
+		// 트랜잭션 전송
+		EthSendTransaction transactionReceipt = transactionManager.sendTransaction(
+				gasProvider.getGasPrice(functionName),
+				gasProvider.getGasLimit(functionName),
+				DOV_ADDRESS,
+				encodedFunction,
+				BigInteger.ZERO);
+		System.out.println("Transaction Hash: " + transactionReceipt.getTransactionHash());
+	}
 }
