63b4857439995a960214c01730c1016098caf626
masonkimseoul <masonkimseoul@gmail.com>
2023-11-16 17:55:01 +0900

    feat : OrderRequestDTO 추가

---
+++ src/main/java/com/DefiOptionVault/DOV/Order/OrderRequestDTO.java
@@ -0,0 +1,49 @@
+package com.DefiOptionVault.DOV.Order;
+
+public class OrderRequestDTO {
+    private Integer amount;
+    private Integer optionId;
+    private String position;
+    private String strikePrice;
+    private String clientAddress;
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Integer getOptionId() {
+        return optionId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getStrikePrice() {
+        return strikePrice;
+    }
+
+    public String getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public void setOptionId(Integer optionId) {
+        this.optionId = optionId;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setStrikePrice(String strikePrice) {
+        this.strikePrice = strikePrice;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+}
